7fb8963fe204148688552ca322ca45d61db33342
Ajinkya <akale881@gmail.com>
2025-02-27 00:21:21 -0500

    adding bootstrap vue next

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(createBootstrap)
 app.mount('#app') 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   server: {
     port: 5173
   },
