9e19e1fd1966722bf852df0ee80cfbe8cccd761a
Ajinkya <aak9225@g.rit.edu>
2025-03-02 15:49:38 -0500

    reverting the app domain

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -1,6 +1,6 @@
 export const SPOTIFY_CONFIG = {
   CLIENT_ID: '9bb5ba34bfe14bcbb093f03a9bbe3e2b',
-  REDIRECT_URI: 'https://spotify-client-28yq1vadc-ajinkyakaleys-projects.vercel.app/callback',
+  REDIRECT_URI: 'http://spotify-client-seven.vercel.app/callback',
   AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
   RESPONSE_TYPE: 'token'
 }; 
